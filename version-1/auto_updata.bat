e5207c9612443d91c94db04e0480a3ff48f8f4e6
chenghao0415 <81077332+chenghao0415@users.noreply.github.com>
2022-05-17 21:52:24 +0800

    Add files via upload

---
+++ version-1/auto_updata.bat
@@ -0,0 +1,56 @@
+@echo off
+
+::goto start
+::如果不要自動更新,刪除上方那行的::
+
+set version=1
+set file_name=auto_updata.bat
+
+title check updata
+mode con lines=5 cols=25
+
+::link
+
+set server_host=localhost
+set server_url=http://localhost:8080
+
+del /f /q %cd%\updata.bat
+cls
+
+ping -n 2 %server_host%>nul
+
+if %errorlevel%==0 (
+	goto updata
+) else (
+	goto start
+)
+
+:updata
+
+echo check version
+
+bitsadmin.exe /transfer "download" %server_url%/version.txt %cd%\version.txt>nul
+
+for /f %%i in (%cd%/version.txt) do set new_version=%%i
+
+if %new_version%==%version% (
+	del /q %cd%\version.txt
+	goto start
+) else (
+	del /q %cd%\version.txt
+	echo @echo off>>%cd%\updata.bat
+	echo title updata>>%cd%\updata.bat
+	echo mode con cols=70 lines=15>>%cd%\updata.bat
+	echo del /q %cd%\%file_name%>>%cd%\updata.bat
+	echo bitsadmin.exe /transfer "download" %server_url%/%new_version%/%file_name% %cd%\%file_name%>>%cd%\updata.bat
+	echo start /d %cd% %cd%\%file_name%>>%cd%\updata.bat
+	echo exit>>%cd%\updata.bat
+	start /d %cd% /min %cd%\updata.bat
+	exit
+)
+
+:start
+cls
+
+echo ya
+pause
